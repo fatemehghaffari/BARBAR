5be1aa099fc7cd97dc068a0b578707b7dd2a4979
fatemehghaffari <47489341+fatemehghaffari@users.noreply.github.com>
2024-01-09 22:11:18 -0500

    Update barbar.py

--- barbar.py
+++ barbar.py
@@ -1,8 +1,8 @@
 import numpy as np 
 from copy import deepcopy
 from time import sleep
-
-def BARBAR(env, means_real, K, T, delta = 0.2):
+from collections import Counter
+def BARBAR(env, means_real, K, T, delta = 0.2, step = 1):
     # lmbda = 1024 * np.log((8 * K / delta) * np.log2(T))
     lmbda = np.log((8 * K / delta) * np.log2(T))
     t = 0
@@ -32,7 +32,7 @@ def BARBAR(env, means_real, K, T, delta = 0.2):
         best = np.argmax(means_real)
         return means_real[best] * t - sum([means_real[actions[i]] for i in range(t)])
 
-    return [regret(t) for t in range(T)]
+    return [regret(t) for t in list(range(T)[0 : T : step])]
         
 def BARBAR_dist_het_agent(env, K, N, pr):
     '''
@@ -60,7 +60,7 @@ def BARBAR_dist_het_agent(env, K, N, pr):
     return ereward, actions
 
 
-def BARBAR_dist_het(env, means_real, L, K, T, delta = 0.2):
+def BARBAR_dist_het(env, means_real, L, K, T, regret_mode = "round", delta = 0.2, step = 1):
     L_list = np.sum(K, axis = 0)
     r = np.zeros([L, len(L_list)])
     rs = np.zeros(L)
@@ -73,6 +73,7 @@ def BARBAR_dist_het(env, means_real, L, K, T, delta = 0.2):
     n = np.zeros([L, k])
     while t < T:
         N_max = 0
+        r = np.zeros([L, len(L_list)])
         for j in range(L):
             n[j] = (lmbda / (Delta[j]) ** 2) * K[j]
             n[np.isnan(n)] = 0
@@ -103,12 +104,60 @@ def BARBAR_dist_het(env, means_real, L, K, T, delta = 0.2):
                 agg_reg += means_real[best[ind]] * l - sum([means_real[all_actions[ind][i]] for i in range(l)])
             else:
                 agg_reg += float(format(means_real[best[ind]] * t, '.3f')) - float(format(sum([means_real[all_actions[ind][i]] for i in range(t)]), '.3f'))
-                # if means_real[best[ind]] * t - sum([means_real[all_actions[ind][i]] for i in range(t)])<0:
-                    # print(t)
-                    # print(means_real[best[ind]] * t)
-                    # print(sum([means_real[all_actions[ind][i]] for i in range(t)]))
-                    # print()
-                    # sleep(.1)
         return agg_reg
+    
+    if regret_mode == "round":
+        return [regret(t) for t in list(range(T)[0 : T : step])]
+    elif regret_mode == "final":
+        return regret(T)
 
-    return [regret(t) for t in range(T)]
+def BARBAR_lf_het(env, means_real, L, K, T, regret_mode = "round", delta = 0.2, step = 1):
+    L_list = np.sum(K, axis = 0)
+    k = K.shape[1]
+    r = np.zeros(k)
+    rs = 0
+    Delta = np.ones(k)
+    m = 1
+    t = 0
+    all_actions = [[] for _ in range(L)]
+    
+    lmbda = np.log((8 * k / delta) * np.log2(T))
+    n = np.zeros(k)
+    while t < T:
+        N_max = 0
+        n = (lmbda / ((Delta) ** 2))
+        r = np.zeros(k)
+        for j in range(L):
+            N = np.sum(n * K[j]/L_list) 
+            N_max = max(N_max, N)
+            pr = (n * K[j]) / (L_list * N)
+            action = np.random.choice(np.array(range(k)), p=pr)
+            pra = np.zeros(k)
+            pra[action] = 1
+            er, actions = BARBAR_dist_het_agent(env, k, N, pra)
+            r += er
+            all_actions[j] += actions
+        t += N_max
+        r = r/n
+        rs = np.max(r - (1/16) * Delta)
+        Delta = np.maximum(2**(-1*m), (rs - r))
+        m += 1    
+    # from collections import Counter
+    # for ind in range(L):
+    #     print(Counter(all_actions[ind]))
+    #     print(len(all_actions[ind]))
+    #     print()
+    def regret(t):
+        agg_reg = 0
+        best = np.argmax(means_real * K, axis = 1)
+        for ind in range(L):
+            if t > len(all_actions[ind]):
+                l = len(all_actions[ind])
+                agg_reg += means_real[best[ind]] * l - sum([means_real[all_actions[ind][i]] for i in range(l)])
+            else:
+                agg_reg += float(format(means_real[best[ind]] * t, '.3f')) - float(format(sum([means_real[all_actions[ind][i]] for i in range(t)]), '.3f'))
+        return agg_reg
+    if regret_mode == "round":
+        return [regret(t) for t in list(range(T)[0 : T : step])]
+    elif regret_mode == "final":
+        return regret(T)
